48eb37fc9d3bf29d5310f4c5152a2a764f599d66
Leo <leonarodsalo11@gmail.com>
2024-02-26 16:28:49 -0700

    Practica 9 parte 1

---
+++ app/src/main/java/mx/edu/itesca/practica8/Cliente.kt
@@ -0,0 +1,6 @@
+package mx.edu.itesca.practica8
+
+data class Cliente(var nombre: String, var tipoPago: String, var asiento: Int)
+{
+
+}
--- app/src/main/java/mx/edu/itesca/practica8/Pelicula.kt
+++ app/src/main/java/mx/edu/itesca/practica8/Pelicula.kt
@@ -1,8 +1,14 @@
 package layout
 
+import mx.edu.itesca.practica8.Cliente
+
 data class Pelicula(
     var titulo:String,
     var image:Int,
     var header:Int,
-    var sinopsis:String
+    var sinopsis:String,
+    var seats: ArrayList<Cliente>
 )
+{
+
+}
